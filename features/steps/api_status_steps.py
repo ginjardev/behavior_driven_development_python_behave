92dd0319f16fb40038538359fff3b9ac2b0de3e4
Ini Arthur <arthurshur@gmail.com>
2025-06-07 20:55:26 +0000

    refactoring

--- features/steps/api_status_steps.py
+++ features/steps/api_status_steps.py
@@ -2,19 +2,19 @@ import requests
 from behave import given, when, then
 
 @given('the API endpoint is "{endpoint}"')
-def step_given_api_endpoint(context, endpoint):
+def step_api_endpoint(context, endpoint):
     context.url = context.base_url + endpoint
 
 @when('I make a GET request')
-def step_when_get_request(context):
+def step_get_request(context):
     context.response = requests.get(context.url)
 
 @then('the response status should be "{status_code}"')
-def step_then_response_status(context, status_code):
+def step_response_status(context, status_code):
     assert context.response.status_code == int(status_code), f'Expected status code {status_code}, but got {context.response.status_code}'
 
 @then('the response should contain the field "{field}"')
-def step_then_response_contains_field(context, field):
+def step_response_contains_field(context, field):
     try:
         json_data = context.response.json()
     except Exception:
